75aa1f474f05f7c8e4445c2d1e5b63a508b2f019
Allo0o2a <31714177+Allo0o2a@users.noreply.github.com>
2018-02-14 04:40:35 -0500

    Update AllaAlharazi_GetGitHubTest.py

--- AllaAlharazi_GetGitHubTest.py
+++ AllaAlharazi_GetGitHubTest.py
@@ -14,7 +14,8 @@ from AllaAlharazi_GetGitHub import get_info
 class TestGetInfo(unittest.TestCase):
     
     def testValidUser(self):
-        self.assertEqual(get_info('allo0o2a'), [('AllaAlharazi_SSW467', 0), ('GitHubApi567', 19), ('Triangle567', 6)])
+        self.assertEqual(get_info('allo0o2a'), [('AllaAlharazi_SSW467', 0), ('GitHubApi567', 21), ('Triangle567', 6)])
+        self.assertEqual(get_info("kevinwlu"), [('autobahn-js-built', 18), ('iot', 30), ('kevinwlu.github.io', 0), ('mpl_finance', 14)])
 
     def testInvalidUser(self):
         self.assertEqual(get_info('jsbfjejknejk'), 'Invalid GitHub Username')
