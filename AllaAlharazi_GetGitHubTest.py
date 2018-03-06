5ff16328c3aa9df343b4dfda90a22e51f11439b7
Allo0o2a <31714177+Allo0o2a@users.noreply.github.com>
2018-03-05 21:13:15 -0500

    Adding mock

--- AllaAlharazi_GetGitHubTest.py
+++ AllaAlharazi_GetGitHubTest.py
@@ -7,17 +7,21 @@ Created on Tue Feb 13 20:32:15 2018
 """
 
 import unittest
-
+from unittest import mock
 from AllaAlharazi_GetGitHub import get_info
 
 
 class TestGetInfo(unittest.TestCase):
-    
-    def testValidUser(self):
+    @mock.patch('AllaAlharazi_GetGitHub.get_info')
+    def testValidUser(self, mockedReq):
+        mockedReq.return_value = [('AllaAlharazi_SSW467', 0), ('GitHubApi567', 27), ('Triangle567', 6)]
         self.assertEqual(get_info('allo0o2a'), [('AllaAlharazi_SSW467', 0), ('GitHubApi567', 27), ('Triangle567', 6)])
+        mockedReq.return_value =  [('autobahn-js-built', 18), ('iot', 30), ('kevinwlu.github.io', 0), ('mpl_finance', 14)]
         self.assertEqual(get_info("kevinwlu"), [('autobahn-js-built', 18), ('iot', 30), ('kevinwlu.github.io', 0), ('mpl_finance', 14)])
 
-    def testInvalidUser(self):
+    @mock.patch('AllaAlharazi_GetGitHub.get_info')    
+    def testInvalidUser(self, mockedReq):
+        mockedReq.return_value = 'Invalid GitHub Username'
         self.assertEqual(get_info('jsbfjejknejk'), 'Invalid GitHub Username')
         self.assertEqual(get_info(' '), 'Invalid GitHub Username')
 
